d93dd21da271057a985f34d9aa6767adda150c3d
Rafael Maia de Souza <rmaiadesouza@gmail.com>
2024-03-03 16:03:37 -0300

    Neste commit temos desenvolvimento inicial para sorteio de número aleatório. Além disso, já temos o início do desenvolvimento do reconhecimento de voz.

---
+++ app/sortearNumero.js
@@ -0,0 +1,18 @@
+
+const menorValor = 12
+const maiorValor = 1000
+const numeroSecreto = gerarNummeroAleatorio()
+
+function gerarNummeroAleatorio(){
+    
+    let numeroSorteado = parseInt(Math.random() * maiorValor + 1)
+    
+    return numeroSorteado;
+     
+}
+
+const elementoMenorValor = document.getElementById('menor-valor')
+const elementoMaiorValor = document.getElementById('maior-valor')
+
+elementoMenorValor.innerHTML = menorValor
+elementoMaiorValor.innerHTML = maiorValor
